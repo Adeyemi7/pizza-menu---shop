0feecb530d6969d010210ab5ab2dbd22d6da6ae4
Adeyemi Ayodele a.k.a RilDev <integraadeyemi11@gmail.com>
2024-01-11 01:32:20 +0100

    on going project with react basics

---
+++ src/README.md
@@ -0,0 +1,30 @@
+React Pizza Shop Project
+Pizzas
+Savor a delightful assortment of pizza flavors, including:
+
+Focaccia: Bread with Italian olive oil and rosemary.
+Pizza Margherita: Tomato and mozzarella.
+Pizza Spinaci: Tomato, mozzarella, spinach, and ricotta cheese.
+Pizza Funghi: Tomato, mozzarella, mushrooms, and onion.
+Pizza Salamino: Tomato, mozzarella, and pepperoni. (Currently sold out)
+Pizza Prosciutto: Tomato, mozzarella, ham, arugula, and burrata cheese.
+Each pizza is accompanied by a detailed description of its ingredients, pricing, and an enticing photo to captivate customers.
+
+Data Structure
+The backbone of the pizza shop is the pizzaData array within the React component (PizzaList.js). This structured data serves as the foundation for each pizza, which is represented as a React component. This approach enables easy management and updates to the pizza information, ensuring a dynamic and engaging user experience.
+
+React Skills
+This project highlights fundamental React skills, including:
+
+Props: Utilizing props to pass data from parent components (PizzaList) to child components (individual pizzas), enabling the dynamic display of information for each pizza.
+
+Key: Implementing the use of keys to uniquely identify and track each dynamically rendered pizza component, optimizing React's rendering process and improving performance.
+
+JSX: Embracing JSX syntax to seamlessly integrate JavaScript logic within the presentation layer, enhancing code readability and maintainability.
+
+Displaying Data: Effectively leveraging React components to display and structure pizza data, creating a visually appealing and organized user interface.
+
+Conditional Rendering: Employing conditional rendering to dynamically display information, such as indicating when a pizza is sold out. This enhances user experience by providing relevant and real-time information.
+
+Contributing
+Join the pizza-loving community by contributing to this React pizza shop project! Whether you're fixing bugs, introducing new features, or enhancing the user interface, your contributions are warmly welcomed. Feel free to fork the repository, make your improvements, and submit a pull request to share your delicious enhancements with the world. Let's continue making this pizza shop even more irresistible together! 🍕🚀
---
+++ src/index.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './data.js';
+import './style.css';
+
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with Italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozzarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozzarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozzarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozzarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozzarella, ham, arugula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
+function App() {
+    return (
+        <div className='container'>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1>Fast React Pizza Co.</h1>
+        </header>
+    );
+}
+
+function Menu() {
+    return (
+        <main className='menu'>
+            <h2>Our Menu</h2>
+            <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+            <ul className='pizzas'>
+                {pizzaData.map((pizza) => (
+                    <Pizza buyPizza={pizza} key={pizza.name} />
+                ))}
+            </ul>
+        </main>
+    );
+}
+
+function Pizza(props) {
+    console.log(props);
+
+    return (
+        <li>
+            <div className='pizza'>
+                <img src={props.buyPizza.photoName} alt={props.buyPizza.name} />
+                <div>
+                    <h3>{props.buyPizza.name}</h3>
+                    <p>{props.buyPizza.ingredients}</p>
+                    <span>{props.buyPizza.price}</span>
+                </div>
+            </div>
+        </li>
+    );
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closedHour = 23;
+    const isOpen = hour >= openHour && hour <= closedHour;
+    console.log(isOpen);
+
+    return (
+        <footer className='footer'>
+            {isOpen && (
+                <div className="order">
+                    <p>We're open until {closedHour}:00. Come visit us or order online.</p>
+                    <button className="btn">Order</button>
+                </div>
+            )}
+        </footer>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
